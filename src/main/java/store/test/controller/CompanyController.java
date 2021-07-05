ac446220cfcc481d56beb0ea05bfe4748bee2067
sagekhw <sagekhw@gmail.com>
2021-07-05 16:37:52 +0900

    test simple code

--- src/main/java/store/test/controller/CompanyController.java
+++ src/main/java/store/test/controller/CompanyController.java
@@ -1,9 +1,31 @@
 package store.test.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import store.test.entity.Company;
+import store.test.repository.CompanyRepository;
+import store.test.service.CompanyService;
 
+import java.util.List;
+
+@RequiredArgsConstructor
 @RestController
 @RequestMapping("/company")
 public class CompanyController {
+
+    private final CompanyService companyService;
+
+    @PostMapping("/save")
+    public String saveCompany(@RequestBody Company company){
+        return companyService.saveCompany(company);
+    }
+
+    @PostMapping("/findAll")
+    public List<Company> findAllCompany(){
+        return companyService.findAll();
+    }
+
 }
--- src/main/java/store/test/service/CompanyService.java
+++ src/main/java/store/test/service/CompanyService.java
@@ -1,7 +1,32 @@
 package store.test.service;
 
 import org.springframework.stereotype.Service;
+import store.test.entity.Company;
+import store.test.repository.CompanyRepository;
+
+import javax.transaction.Transactional;
+import java.util.List;
 
 @Service
 public class CompanyService {
+    private final CompanyRepository companyRepository;
+
+    public CompanyService(CompanyRepository companyRepository){
+        this.companyRepository = companyRepository;
+    }
+
+    @Transactional
+    public String saveCompany(Company company){
+        Company result = companyRepository.save(company);
+        if(result.getName() != null){
+            return "success";
+        }else{
+            return "false";
+        }
+    }
+
+    @Transactional
+    public List<Company> findAll(){
+        return companyRepository.findAll();
+    }
 }
